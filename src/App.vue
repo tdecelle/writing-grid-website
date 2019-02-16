38526f5f47d7b8911f72b093272351693d8e13f0
Max <maxdrob1@gmail.com>
2019-02-16 22:15:56 +0000

    Added nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,55 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Writing Grid</router-link>
-      <router-link to="/listings">listings</router-link>
-      <router-link to="/blog">Blog</router-link>
-      <router-link to="/login">Login</router-link>
-    </div>
+    <ul id="nav">
+      <li class="nav_left"><router-link to="/">Writing Grid</router-link></li>
+      <li class="nav_right"><router-link to="/listings">Listings</router-link></li>
+      <li class="nav_right"><router-link to="/blog">Blog</router-link></li>
+      <li class="nav_right"><router-link to="/login">Login</router-link></li>
+    </ul>
     <router-view/>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-  padding: 10px;
-}
+  #nav {
+    padding: 0px;
+    list-style-type: none;
+    margin: 0;
+    overflow: hidden;
+    background-color: #4286f4;
+  }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  #nav a {
+    font-weight: bold;
+    padding: 30px;
+    font-size: 20;
+    display: block;
+    color: white;
+    text-align: center;
+    text-decoration: none;
+  }
+  
+  .nav_left {
+    float:left;
+  }
+  
+  .nav_right{
+    float:right;
+  }
+  
+  #nav a:hover {
+    background-color: #3e53a5;
+  }
+
+  #nav a.router-link-exact-active {
+    background-color: #4fc14d;
+  }
 </style>
