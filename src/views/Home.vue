e75c5bb8761217e8df168afa150a25cc07fa641a
tdecelle <tdecelle@gmail.com>
2019-02-16 15:03:48 -0700

    Added the Homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Homepage msg="Max's slogan should go here"/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Homepage from '@/components/Homepage.vue'; // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld,
+    Homepage,
   },
 })
 export default class Home extends Vue {}
