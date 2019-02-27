630a1d8e0313ac74fb8df84ed7bd90b37852b0f5
tdecelle <tdecelle@gmail.com>
2019-02-27 16:43:09 -0700

    Added List Components to Homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,31 @@
 <template>
   <div class="home">
+    <List name="Top Edits" type="edit" :json="edits"/>
     <Homepage/>
+    <List name="Top Writers" type="writer" :json="writers"/>
+    <List name="Top Listings" type="listing" :json="listings" />
   </div>
 </template>
 
-<script lang="ts">
+<script>
   import { Component, Vue } from 'vue-property-decorator';
   import Homepage from '@/components/Homepage.vue'; // @ is an alias to /src
+  import List from '@/components/List.vue';
+  import ListingsJSON from '@/data/listings.json';
+  import EditsJSON from '@/data/edits.json';
+  import WritersJSON from '@/data/writers.json';
 
   @Component({
     components: {
       Homepage,
+      List,
+    },
+    data: () => {
+      return {
+        writers: WritersJSON.writers,
+        listings: ListingsJSON.listings,
+        edits: EditsJSON.edits,
+      };
     },
   })
   export default class Home extends Vue {}
